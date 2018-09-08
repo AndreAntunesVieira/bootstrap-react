8de9a1da376640e1fe0c09180e6c462d732d84ea
Andre Antunes Vieira <andreantunesv@gmail.com>
2018-09-08 12:56:39 -0300

    Refactoring

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const routes = require('./pages/_routes')
 const dev = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 3000
@@ -6,45 +5,17 @@ const fastify = require('fastify')
 const Next = require('next')
 const app = Next({ dev })
 const handler = routes.getRequestHandler(app)
-const babel = require('babel-core')
 
-app.prepare()
-  .then(() => {
-    const server = fastify()
+openServer()
 
-    server.get('/samples/*', (req, res) => {
-      if (isJs(req)) {
-        return sendJs(req, res)
-      }
-      return sendHtml(req, res)
-    })
-
-    server.get('/*', (req, res) => handler(req.req, res.res))
-
-    server.listen(port, (err) => {
-      if (err) throw err
-      console.log(`> Ready on http://localhost:${port}`)
-    })
-  })
-
-
-function isJs (req) {
-  const path = req.params['*']
-  return /.*\.js$/.test(path)
+async function openServer(){
+  await app.prepare()
+  const server = fastify()
+  server.get('/*', (req, res) => handler(req.req, res.res))
+  server.listen(port, serverFeedback)
 }
 
-function sendJs (req, res) {
-  res.type('text/plain; charset=utf-8').code(200)
-  const path = req.params['*']
-  return fs.readFile(`${__dirname}/samples/${path}`, 'utf8', (e, content) => {
-    return res.send(babel.transform(content, { presets: 'react' }).code)
-  })
-}
-
-function sendHtml (req, res) {
-  res.type('text/html').code(200)
-  const path = req.params['*']
-  return fs.readFile(`${__dirname}/samples/${path}/index.html`, 'utf8', (e, content) => {
-    return res.send(content)
-  })
+function serverFeedback(err){
+  if (err) throw err
+  console.log(`> Ready on http://localhost:${port}`)
 }
